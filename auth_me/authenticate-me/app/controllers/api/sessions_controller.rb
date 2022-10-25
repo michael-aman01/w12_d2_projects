5fb049602de406a40cf1bfff2ef820a3b1ed0066
Mitchell Jindra <mitchjindra413@gmail.com>
2022-10-25 16:02:13 -0700

    modify params

--- auth_me/authenticate-me/app/controllers/api/sessions_controller.rb
+++ auth_me/authenticate-me/app/controllers/api/sessions_controller.rb
@@ -9,13 +9,14 @@ class Api::SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by_credentials(params[:user][:credential], params[:user][:password])
+    @user = User.find_by_credentials(params[:credential], params[:password])
+    debugger
     if @user
-          login!(@user)
-          render 'api/users/show'
-        else
-          render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
-        end
+      login!(@user)
+      render 'api/users/show'
+    else
+      render json: { errors: ['The provided credentials were invalid.'] }, status: :unauthorized
+    end
   end
 
   def destroy
